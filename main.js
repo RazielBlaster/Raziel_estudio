14be8d4976f4eceb7c56759b7db68abacdae8ca9
RazielBlaster <david.bass0209@gmail.com>
2023-03-20 21:10:05 -0500

    conexión equipo Raziel

---
+++ main.js
@@ -0,0 +1,87 @@
+//vARIABLES
+{
+// let nombre = "Raziel"
+
+// //console.log(nombre);
+
+// let nombre1 ="Yin"
+
+// //forma de concatenar
+// console.log(nombre + " "+ nombre1);  // concatenar 1
+// console.log(nombre,nombre1);  // concatena y da espacio automatico 
+// console.log(`${nombre} y ${nombre1} \n son gays`);  // template string \n= salto de linea
+}
+
+
+
+//para comentar varias lineas ctrl+k+c y descomentar ctrl+k+u
+
+// let perra = "yo"
+
+// console.log(typeof(perra));//typeof saber que tipo de dato es
+
+
+//OPERADORES
+{
+    // console.log(20+4) //suma 
+// console.log(20-4) //resta
+// console.log(20*4) //Multiplicar
+// console.log(20/8) //división
+// console.log(20**4) //exponenciacion
+// console.log(20%4) //Residuo de la división
+}
+
+// let num= 3
+// /*
+// num++//incremento 
+// num--// decremento
+// */
+// // num=num+1
+// // num+=5 //Adicion mas asignación
+// // num-=1 //sustración mas asignación
+// console.log(num)
+
+// let num1 = "3"
+// let num2 = 3
+
+// if(num1 === num2){
+//     console.log('igual');
+// }else{
+//     console.log(' No es');
+// }
+// function suma() {
+//     let num1= 2
+//     let num2= 4
+//     let resul= num1+num2
+//     return resul    
+// }
+// let totalSum =  suma();
+
+// totalSum += 4
+// console.log(totalSum);
+
+// function multiplicar(num1,num2){
+
+//     let resul = num1*num2
+//     return resul 
+// }
+// console.log(multiplicar(4,6)); 
+
+
+// let nameUser ="Raziel"
+// let age = 4
+// let genre = "woman"
+
+// function concatenar(nameUser,age,genre){
+//     console.log(`El usuario ${nameUser} tiene ${age} años y es de genero ${genre}`);
+// }
+// concatenar(nameUser,age,genre)
+
+// let num1 = 20
+// let num2 = 40
+// let num3 = 10
+// function oper(num1,num2,num3){
+//     let resul= (num1+num2)/num3
+//     return resul
+// }
+// console.log(oper(num1,num2,num3));
---
+++ semana2.js
@@ -0,0 +1,108 @@
+
+//metodos de string
+
+//length = muestra la longitud de algo (Cualquier tipo de dato)
+
+{
+ // let frase = "Jhojan es"
+// let contar= frase.length
+// console.log(contar);
+
+
+// const nMax = 10
+
+// let word = 'la pija nos vemos'
+
+// if (word.length > nMax) {
+//     console.log(' Te pasaste de verga');
+// }else{
+//     console.log('Perfecto');
+// }
+
+// console.log(typeof word.length)
+}
+
+
+//includes = En una cadena de texto buscar lo que se esta solicitando(es sensible a mayus y minus)
+{
+// let frase1= "La vida es una verga"
+// let caract= "La"
+
+// console.log(frase1.includes(caract));
+}
+
+
+//toUpperCase = para cambiar a mayusculas
+//toLowerCase = para cambiar a minus
+{
+// let frase1 = "la perra de jhojan"
+// console.log(frase1.toUpperCase());
+// let frase2 ="MAYUCULAS"
+// console.log(frase2.toLowerCase());
+
+// // si una letra esta dentro de frase1 y se cumple alguna de las condicion 
+// //frase2 = frase2.toLowerCase()
+// console.log(frase2.includes("a"));
+// console.log(frase2.toLowerCase().includes("a"));
+
+}
+
+//replace = para reempazar un caracter
+{
+// let frase = "Hola mundo mama"
+// let palabra ="perra"
+// console.log(frase.replace("m","M"));
+// console.log(frase.replaceAll("m","M").toUpperCase().replace("A","a"));
+}
+
+//slice = cortar
+{
+// let frase="Hola manada de pervertidos"
+// console.log(frase.slice(0,6));
+// console.log(frase.slice(-5));
+}
+
+//endsWith y startsWith para validar si empieza con o terminar con
+
+{
+// let frase="hola perra"
+// console.log(frase.endsWith("a"));
+// console.log(frase.startsWith("j"));
+}
+//if = condicional 
+
+{
+//let frase="hola perra"
+// if(frase.includes("z")){
+// console.log("Si esta la letra a");
+// }else{
+//     console.log("No esta la letra a");
+// }
+
+// if(frase.includes("r")){
+//     frase=frase.replaceAll("r","R")
+//     console.log(frase);
+// }
+
+
+//Cree una función en donde si la frase tiene menos de 15 letras remplace la última letra por una mayúscula, si tiene más de 15 letras. Remplace la primera letra por minúscula.
+
+let frase="hola a todos"
+
+function change(frase){
+if(frase.length <= 15){
+  let guarLet=frase.slice(-1)
+  let camMayus=guarLet.toUpperCase()
+  if(frase.includes(guarLet)){
+    frase=frase.replace(guarLet,camMayus)
+     console.log(frase)
+  }
+ 
+}else{
+  console.log("no funciono")
+}  
+}
+
+
+change(frase)
+}
